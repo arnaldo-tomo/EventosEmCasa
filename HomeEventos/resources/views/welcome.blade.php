725da363dc360d4c3d90414ff04ebc1861c43973
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2022-11-30 15:15:32 +0200

    welcome

--- HomeEventos/resources/views/welcome.blade.php
+++ HomeEventos/resources/views/welcome.blade.php
@@ -751,98 +751,102 @@
                     </div>
                 </div>
                 <div class="row g-4">
-                    <div class="col-sm-6 col-lg-3">
-                        <!-- Event item START -->
-                        <div class="card h-100">
-                            <div class="position-relative">
-                                <img class="img-fluid rounded-top" src="assets/images/events/01.jpg" alt="">
-                                <div class="badge bg-danger text-white mt-2 me-2 position-absolute top-0 end-0">
-                                    Online
+                    @foreach ($eventos as $evento)
+                        <div class="col-sm-6 col-lg-3">
+                            <!-- Event item START -->
+                            <div class="card h-100">
+                                <div class="position-relative">
+                                    <img class="img-fluid rounded-top" src="/{{ $evento->imagen }}" alt="">
+                                    <div class="badge bg-danger text-white mt-2 me-2 position-absolute top-0 end-0">
+                                        {{ $evento->localizacao }}
+                                    </div>
                                 </div>
-                            </div>
-                            <!-- Card body START -->
-                            <div class="card-body position-relative pt-0">
-                                <!-- Tag -->
-                                <a class="btn btn-xs btn-primary mt-n3" href="{{ route('detalhes') }}">Treinamento em
-                                    spa
-                                </a>
-                                <h5 class="mt-3"> <a href="{{ route('detalhes') }}"> Bone thugs-n-harmony </a>
-                                </h5>
-                                <!-- Data hora -->
-                                <p class="mb-0 small"> <i class="bi bi-calendar-check pe-1"></i> segunda-feira, 25
-                                    de
-                                    setembro de 2020 às
-                                    9h30 </p>
-                                <p class="small"> <i class="bi bi-geo-alt pe-1"></i> São Francisco </p>
-                                <!-- Avatar group START -->
-                                <ul class="avatar-group list-unstyled align-items-center mb-0">
-                                    <li class="avatar avatar-xs">
-                                        <img class="avatar-img rounded-circle" src="assets/images/avatar/01.jpg"
-                                            alt="avatar">
-                                    </li>
-                                    <li class="avatar avatar-xs">
-                                        <img class="avatar-img rounded-circle" src="assets/images/avatar/03.jpg"
-                                            alt="avatar">
-                                    </li>
-                                    <li class="avatar avatar-xs">
-                                        <img class="avatar-img rounded-circle" src="assets/images/avatar/04.jpg"
-                                            alt="avatar">
-                                    </li>
-                                    <li class="avatar avatar-xs">
-                                        <div class="avatar-img rounded-circle bg-primary"><span
-                                                class="menor text-white position-absolute top-50 start-50 translate-middle">+78</span>
+                                <!-- Card body START -->
+                                <div class="card-body position-relative pt-0">
+                                    <!-- Tag -->
+                                    <a class="btn btn-xs btn-primary mt-n3"
+                                        href="{{ route('detalhes') }}">Treinamento em
+                                        spa
+                                    </a>
+                                    <h5 class="mt-3"> <a href="{{ route('detalhes') }}"> Bone thugs-n-harmony </a>
+                                    </h5>
+                                    <!-- Data hora -->
+                                    <p class="mb-0 small"> <i class="bi bi-calendar-check pe-1"></i> segunda-feira, 25
+                                        de
+                                        setembro de 2020 às
+                                        9h30 </p>
+                                    <p class="small"> <i class="bi bi-geo-alt pe-1"></i> São Francisco </p>
+                                    <!-- Avatar group START -->
+                                    <ul class="avatar-group list-unstyled align-items-center mb-0">
+                                        <li class="avatar avatar-xs">
+                                            <img class="avatar-img rounded-circle" src="assets/images/avatar/01.jpg"
+                                                alt="avatar">
+                                        </li>
+                                        <li class="avatar avatar-xs">
+                                            <img class="avatar-img rounded-circle" src="assets/images/avatar/03.jpg"
+                                                alt="avatar">
+                                        </li>
+                                        <li class="avatar avatar-xs">
+                                            <img class="avatar-img rounded-circle" src="assets/images/avatar/04.jpg"
+                                                alt="avatar">
+                                        </li>
+                                        <li class="avatar avatar-xs">
+                                            <div class="avatar-img rounded-circle bg-primary"><span
+                                                    class="menor text-white position-absolute top-50 start-50 translate-middle">+78</span>
+                                            </div>
+                                        </li>
+                                        <li class="ms-3">
+                                            <small> estão participando</small>
+                                        </li>
+                                    </ul>
+                                    <!-- Avatar group END -->
+                                    <!-- Button -->
+                                    <div class="d-flex mt-3 justify-content-between">
+                                        <!-- Interested button -->
+                                        <div class="w-100">
+                                            <input type="checkbox" class="btn-check d-block" id="Interested1">
+                                            <label class="btn btn-sm btn-outline-success d-block" for="Interested1"><i
+                                                    class="fa-solid fa-thumbs-up me-1"></i> Interessante</label>
                                         </div>
-                                    </li>
-                                    <li class="ms-3">
-                                        <small> estão participando</small>
-                                    </li>
-                                </ul>
-                                <!-- Avatar group END -->
-                                <!-- Button -->
-                                <div class="d-flex mt-3 justify-content-between">
-                                    <!-- Interested button -->
-                                    <div class="w-100">
-                                        <input type="checkbox" class="btn-check d-block" id="Interested1">
-                                        <label class="btn btn-sm btn-outline-success d-block" for="Interested1"><i
-                                                class="fa-solid fa-thumbs-up me-1"></i> Interessante</label>
-                                    </div>
-                                    <div class="dropdown ms-3">
-                                        <a href="#" class="btn btn-sm btn-primary-soft" id="eventActionShare"
-                                            data-bs-toggle="dropdown" aria-expanded="false">
-                                            <i class="bi bi-share-fill"></i>
-                                        </a>
-                                        <!-- Dropdown menu -->
-                                        <ul class="dropdown-menu dropdown-menu-end"
-                                            aria-labelledby="eventActionShare">
-                                            <li><a class="dropdown-item" href="#"> <i
-                                                        class="bi bi-envelope fa-fw pe-1"></i> Enviar via Direct
-                                                    Mensagem</a></li>
-                                            <li><a class="dropdown-item" href="#"> <i
-                                                        class="bi bi-bookmark-check fa-fw pe-1"></i> Compartilhar
-                                                    no
-                                                    feed de notícias
-                                                </a></li>
-                                            <li><a class="dropdown-item" href="#"> <i
-                                                        class="bi bi-pessoas fa-fw pe-1"></i> Compartilhar em um
-                                                    grupo</a></li>
-                                            <li><a class="dropdown-item" href="#"> <i
-                                                        class="bi bi-share fa-fw pe-1"></i> Compartilhar postagem
-                                                    via
-                                                    …</a></li>
-                                            <li>
-                                                <hr class="divisor suspenso">
-                                            </li>
-                                            <li><a class="dropdown-item" href="#"> <i
-                                                        class="bi bi-person fa-fw pe-1"></i> Compartilhar no de um
-                                                    amigo perfil</a></li>
-                                        </ul>
+                                        <div class="dropdown ms-3">
+                                            <a href="#" class="btn btn-sm btn-primary-soft"
+                                                id="eventActionShare" data-bs-toggle="dropdown"
+                                                aria-expanded="false">
+                                                <i class="bi bi-share-fill"></i>
+                                            </a>
+                                            <!-- Dropdown menu -->
+                                            <ul class="dropdown-menu dropdown-menu-end"
+                                                aria-labelledby="eventActionShare">
+                                                <li><a class="dropdown-item" href="#"> <i
+                                                            class="bi bi-envelope fa-fw pe-1"></i> Enviar via Direct
+                                                        Mensagem</a></li>
+                                                <li><a class="dropdown-item" href="#"> <i
+                                                            class="bi bi-bookmark-check fa-fw pe-1"></i> Compartilhar
+                                                        no
+                                                        feed de notícias
+                                                    </a></li>
+                                                <li><a class="dropdown-item" href="#"> <i
+                                                            class="bi bi-pessoas fa-fw pe-1"></i> Compartilhar em um
+                                                        grupo</a></li>
+                                                <li><a class="dropdown-item" href="#"> <i
+                                                            class="bi bi-share fa-fw pe-1"></i> Compartilhar postagem
+                                                        via
+                                                        …</a></li>
+                                                <li>
+                                                    <hr class="divisor suspenso">
+                                                </li>
+                                                <li><a class="dropdown-item" href="#"> <i
+                                                            class="bi bi-person fa-fw pe-1"></i> Compartilhar no de um
+                                                        amigo perfil</a></li>
+                                            </ul>
+                                        </div>
                                     </div>
                                 </div>
+                                <!-- Card body END -->
                             </div>
-                            <!-- Card body END -->
+                            <!-- Event item END -->
                         </div>
-                        <!-- Event item END -->
-                    </div>
+                    @endforeach
                     {{-- <div class="col-sm-6 col-lg-3">
                         <!-- Event item START -->
                         <div class="card h-100">
@@ -1118,6 +1122,7 @@
                         </div>
                         <!-- Event item END -->
                     </div> --}}
+
                     <a href="/Todos" role="button" class="btn btn-loader btn-primary-soft">
                         <span class="load-text"> Ver todos os eventos </span>
                         <div class="load-icon">
