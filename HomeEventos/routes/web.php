7c6fc3859d98aca96e531e9274846207955da246
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2022-12-01 12:24:31 +0200

    Yet

--- HomeEventos/routes/web.php
+++ HomeEventos/routes/web.php
@@ -4,28 +4,20 @@ use App\Http\Controllers\EventosController;
 use App\Models\Eventos;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     $eventos = Eventos::all();
     return view('welcome', compact('eventos'));
 })->name('home');
+
+
+Route::group(['middleware' => ['web']], function () {
+
+    Route::get('/perfil', [EventosController::class, 'perfil'])->middleware()->name('perfil');
+});
 Route::get('/Todos', [EventosController::class, 'todos'])->name('todos');
 Route::get('/Detalhes', [EventosController::class, 'detalhes'])->name('detalhes');
 Route::get('/Categoria', [EventosController::class, 'categoria'])->name('categoria');
-Route::get('/sair', [EventosController::class, 'sair'])->name('sair');
-Route::post('/salvar', [EventosController::class, 'salvar'])->name('salvar');
-
-
-
+Route::get('/Terminar', [EventosController::class, 'sair'])->name('sair');
+Route::post('/Salvar', [EventosController::class, 'salvar'])->name('salvar');
 
 require __DIR__ . '/auth.php';
