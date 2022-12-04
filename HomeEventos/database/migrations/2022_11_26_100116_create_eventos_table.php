64df73f071e260a839b60152bb547c3d3e5e1bee
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2022-12-04 11:02:02 +0200

    nihdvsdhv

--- HomeEventos/database/migrations/2022_11_26_100116_create_eventos_table.php
+++ HomeEventos/database/migrations/2022_11_26_100116_create_eventos_table.php
@@ -17,7 +17,7 @@ class CreateEventosTable extends Migration
             $table->id();
             $table->string('titulo');
             $table->foreignId('user_id')->constrained();
-            $table->string('descricao');
+            $table->string('descricao', 1000);
             $table->string('cidade');
             $table->boolean('privado');
             $table->string('tipo_categoria');
