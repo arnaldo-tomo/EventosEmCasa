7fe96459aa0e11ac12ec9e29ecfd96d773fe3822
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2022-12-04 13:04:57 +0200

    users

--- HomeEventos/routes/web.php
+++ HomeEventos/routes/web.php
@@ -10,5 +10,5 @@ Route::get('/Todos', [EventosController::class, 'todos'])->name('todos');
 Route::get('/Categoria', [EventosController::class, 'categoria'])->name('categoria');
 Route::get('/Terminar', [EventosController::class, 'sair'])->name('sair');
 Route::post('/Salvar', [EventosController::class, 'salvar'])->name('salvar');
-
+Route::post('Participar/{id}', [EventosController::class, 'participar'])->name('participar');
 require __DIR__ . '/auth.php';
