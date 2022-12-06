23a0e8f0e58d40c15b4812f1c83539a601bdfd42
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2022-12-06 23:07:11 +0200

    Update web.php

--- HomeEventos/routes/web.php
+++ HomeEventos/routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', [EventosController::class, 'home'])->name('home');
 Route::get('/Perfil', [EventosController::class, 'perfil'])->middleware(['auth'])->name('perfil');
 Route::get('/Sobre', [EventosController::class, 'sobre'])->middleware(['auth'])->name('sobre');
 Route::get('/Todos', [EventosController::class, 'todos'])->name('todos');
-Route::get('/Categoria', [EventosController::class, 'categoria'])->name('categoria');
+Route::get('/Categoria/{categoria}', [EventosController::class, 'categoria'])->name('categoria');
 Route::get('/Terminar', [EventosController::class, 'sair'])->name('sair');
 Route::post('/Salvar', [EventosController::class, 'salvar'])->name('salvar');
 Route::post('Participar/{id}', [EventosController::class, 'participar'])->name('participar');
