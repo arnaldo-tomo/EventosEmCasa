4a2819b2248fd6105d1e1a2e434932879c8ee6e0
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2022-12-20 18:30:39 +0200

    Update EventosController.php

--- HomeEventos/app/Http/Controllers/EventosController.php
+++ HomeEventos/app/Http/Controllers/EventosController.php
@@ -87,31 +87,35 @@ class EventosController extends Controller
 
     public function salvar(Request $request)
     {
-        $evento = new Eventos();
-        $evento->user_id = Auth::user()->id;
-        $evento->user_name = Auth::user()->name;
-        $evento->titulo = $request->nome;
-        $evento->descricao = $request->descricao;
-        $evento->dataInicio = $request->dataInicio;
-        $evento->dataFim = $request->dataFim;
-        $evento->hora = $request->hora;
-        $evento->privado = $request->privado;
-        $evento->duracao = $request->duracao;
-        $evento->localizacao = $request->localizacao;
-        $evento->cidade = $request->cidade;
-        $evento->tipo_categoria = $request->tipo_categoria;
-        $evento->participante = $request->participante;
-        $evento->imagen = $request->imagen;
-        if ($request->file('imagen') != null) {
-            $filename = $request->file('imagen')->getClientOriginalName();
-            $link = "imagen/eventos/" . $filename;
-            $evento->imagen = $link;
-            $foto = $request->file('imagen');
-            $foto->move('imagen/eventos', $filename);
-        }
-        $evento->save();
-        $messagen = "Evento publicado com sucessos!";
-        return redirect()->back()->with('novo', $messagen);
+
+        dd($request->all());
+        // $evento = new Eventos();
+        // $evento->user_id = Auth::user()->id;
+        // $evento->user_name = Auth::user()->name;
+        // $evento->titulo = $request->nome;
+        // $evento->descricao = $request->descricao;
+        // $evento->dataInicio = $request->dataInicio;
+        // $evento->dataFim = $request->dataFim;
+        // $evento->hora = $request->hora;
+        // $evento->privado = $request->privado;
+        // $evento->duracao = $request->duracao;
+        // $evento->localizacao = $request->localizacao;
+        // $evento->cidade = $request->cidade;
+        // $evento->tipo_categoria = $request->tipo_categoria;
+        // $evento->participante = $request->participante;
+        // $evento->anexo = $request->anexo;
+        // $evento->link = $request->link;
+        // $evento->imagen = $request->imagen;
+        // if ($request->file('imagen') != null) {
+        //     $filename = $request->file('imagen')->getClientOriginalName();
+        //     $link = "imagen/eventos/" . $filename;
+        //     $evento->imagen = $link;
+        //     $foto = $request->file('imagen');
+        //     $foto->move('imagen/eventos', $filename);
+        // }
+        // $evento->save();
+        // $messagen = "Evento publicado com sucessos!";
+        // return redirect()->back()->with('novo', $messagen);
     }
 
     public function gosto($id, Request $request)
