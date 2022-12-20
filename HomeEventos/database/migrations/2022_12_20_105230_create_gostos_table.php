4aa44f99090adea9baf922675aa5794ff2a2c5a4
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2022-12-20 12:46:46 +0200

    Update 2022_12_20_105230_create_gostos_table.php

--- HomeEventos/database/migrations/2022_12_20_105230_create_gostos_table.php
+++ HomeEventos/database/migrations/2022_12_20_105230_create_gostos_table.php
@@ -15,7 +15,6 @@ class CreateGostosTable extends Migration
     {
         Schema::create('gostos', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
         });
     }
 
