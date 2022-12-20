6ff903ea35efa6af1053c22392f73308ee5a91ac
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2022-12-20 12:46:42 +0200

    Update web.php

--- HomeEventos/routes/web.php
+++ HomeEventos/routes/web.php
@@ -12,7 +12,7 @@ Route::get('/Todos', [EventosController::class, 'todos'])->name('todos');
 Route::get('/Categoria/{categoria}', [EventosController::class, 'categoria'])->name('categoria');
 Route::get('/Terminar', [EventosController::class, 'sair'])->name('sair');
 Route::post('/Salvar', [EventosController::class, 'salvar'])->name('salvar');
-// Route::post('evebtos/join/{id}', [EventosController::class, 'participar'])->name('participar');
+Route::post('gosto{id}', [EventosController::class, 'gosto'])->name('gosto');
 Route::post('join/{id}', [EventosController::class, 'join'])->name('join');
 Route::get('/Save', [AdminController::class, 'categoria'])->name('Save');
 require __DIR__ . '/auth.php';
