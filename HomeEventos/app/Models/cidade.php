81402f476ea7bada3ae0796cc7a89c3cf21ac6ed
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2022-12-21 15:38:07 +0200

    Update cidade.php

--- HomeEventos/app/Models/cidade.php
+++ HomeEventos/app/Models/cidade.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class cidade extends Model
 {
-    use HasFactory;
+
+    use HasApiTokens, HasFactory, Notifiable;
     protected $table = 'cidade';
     protected $fillable = ['nome'];
 }
