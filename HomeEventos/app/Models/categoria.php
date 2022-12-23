58f25a5b6df89d378bd2ce69b7250819067443f5
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2022-12-24 01:49:18 +0200

    Update categoria.php

--- HomeEventos/app/Models/categoria.php
+++ HomeEventos/app/Models/categoria.php
@@ -2,16 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Eventos;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class categoria extends Model
 {
     use HasFactory;
+    protected $table = 'categoria';
     protected  $fillable = ['nome', 'descricao', 'foto'];
 
     public function Evento()
     {
         return $this->belongsToMany(Eventos::class,);
+    }
+
+    public function eventoo()
+    {
+        return $this->belongsToMany(Eventos::class, 'evento_categoria', 'evento_id', 'categoria_id');
     }
 }
