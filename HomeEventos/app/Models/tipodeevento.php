7c8fe29086903ac13c8dfcf3298ca42484ded81b
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2022-12-24 13:37:13 +0200

    Update tipodeevento.php

--- HomeEventos/app/Models/tipodeevento.php
+++ HomeEventos/app/Models/tipodeevento.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class tipodeevento extends Model
 {
     use HasFactory;
+
+    public function categoria()
+    {
+        return $this->belongsToMany(categoria::class, 'categoria_tipodeeventos', 'categoria_id', 'tipodeevento_id');
+    }
 }
