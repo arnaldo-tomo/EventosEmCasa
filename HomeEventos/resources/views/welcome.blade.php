f12743f8da3432df2edd33843ad6b370d782fd5d
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2022-12-25 16:24:23 +0200

    Update welcome.blade.php

--- HomeEventos/resources/views/welcome.blade.php
+++ HomeEventos/resources/views/welcome.blade.php
@@ -485,17 +485,22 @@
                             aria-label="Close"></button>
                     </div>
                     <!-- Modal feed header END -->
+
+                    @if ($errors->any())
+                        @foreach ($errors->all() as $error)
+                            <div class="alert alert-danger role">
+                                <div class="display">
+                                    <strong> {{ $error }}</strong>
+                                </div>
+                            </div>
+                        @endforeach
+                    @endif
                     <!-- Modal feed body START -->
                     <div class="modal-body">
                         <!-- Form START -->
-
-
                         <form method="POST" action="{{ url('/Salvar') }}" enctype="multipart/form-data"
                             class="row g-4" id="file-upload-form" class="uploader">
                             @csrf
-
-
-
                             <!-- Title -->
                             <div class="col-12">
                                 <label class="form-label">Titulo</label>
