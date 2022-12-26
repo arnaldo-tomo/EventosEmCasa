8943a9a21c8c040afb79b77e736b906673403f52
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2022-12-26 13:00:19 +0200

    Update welcome.blade.php

--- HomeEventos/resources/views/welcome.blade.php
+++ HomeEventos/resources/views/welcome.blade.php
@@ -996,7 +996,7 @@
                                         <h6 class="modal-title text-white p-2" id="feedActionPhotoLabel">
                                             {{ $evento->titulo }}
                                         </h6>
-                                        <a><a href=""
+                                        <a><a href="{{ $evento->imagen }}" download="{{ $evento->imagen }}"
                                                 class="bi bi-file-earmark-arrow-down text-success  pe-2 p-2">
                                                 Descarregar
                                                 Capa</a></a>
@@ -1012,8 +1012,60 @@
                                     <!-- foto -->
                                     <div class="card  border-1">
                                         <img src="{{ $evento->imagen }}" alt="">
+
                                     </div>
                                     <!--foto  -->
+
+                                    <!-- Card Footer START -->
+                                    <div class="card-footer py-3">
+                                        <!-- Feed react START -->
+                                        <ul class="nav nav-fill nav-stack small">
+                                            <li class="nav-item">
+                                                <a class="nav-link mb-0 active" href="#!"> <i
+                                                        class="bi bi-heart pe-1"></i>Liked (56)</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link mb-0" href="#!"> <i
+                                                        class="bi bi-chat-fill pe-1"></i>Comments (12)</a>
+                                            </li>
+                                            <!-- Card share action dropdown START -->
+                                            <li class="nav-item dropdown">
+                                                <a href="#" class="nav-link mb-0" id="feedActionShare7"
+                                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                                    <i class="bi bi-reply-fill flip-horizontal ps-1"></i>Share (3)
+                                                </a>
+                                                <!-- Card share action dropdown menu -->
+                                                <ul class="dropdown-menu dropdown-menu-end"
+                                                    aria-labelledby="feedActionShare7">
+                                                    <li><a class="dropdown-item" href="#"> <i
+                                                                class="bi bi-envelope fa-fw pe-2"></i>Send via Direct
+                                                            Message</a></li>
+                                                    <li><a class="dropdown-item" href="#"> <i
+                                                                class="bi bi-bookmark-check fa-fw pe-2"></i>Bookmark
+                                                        </a></li>
+                                                    <li><a class="dropdown-item" href="#"> <i
+                                                                class="bi bi-link fa-fw pe-2"></i>Copy link to post</a>
+                                                    </li>
+                                                    <li><a class="dropdown-item" href="#"> <i
+                                                                class="bi bi-share fa-fw pe-2"></i>Share post via …</a>
+                                                    </li>
+                                                    <li>
+                                                        <hr class="dropdown-divider">
+                                                    </li>
+                                                    <li><a class="dropdown-item" href="#"> <i
+                                                                class="bi bi-pencil-square fa-fw pe-2"></i>Share to
+                                                            News Feed</a></li>
+                                                </ul>
+                                            </li>
+                                            <!-- Card share action dropdown END -->
+                                            <li class="nav-item">
+                                                <a class="nav-link mb-0" href="#!"> <i
+                                                        class="bi bi-send-fill pe-1"></i>Send</a>
+                                            </li>
+                                        </ul>
+                                        <!-- Feed react END -->
+                                    </div>
+
 
                                 </div>
                             </div>
