1e7cbed615be4431e5523fc9b650270194eb9bd8
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2022-12-31 09:22:32 +0200

    Update EventosController.php

--- HomeEventos/app/Http/Controllers/EventosController.php
+++ HomeEventos/app/Http/Controllers/EventosController.php
@@ -67,6 +67,7 @@ class EventosController extends Controller
     {
         $dia = Carbon::now();
         $meuID = Auth::user()->id;
+        $users = DB::table('eventos')->where('votes', '>', 100)->get();
         $local = Eventos::where('user_id', $meuID)->ORDERBY('id', 'DESC')->get();
         $online = Eventos::where('categoria_id', 22)->where('user_id', Auth::user()->id)->ORDERBY('id', 'DESC')->get();
         $estaSemana = Eventos::where('created_at',  $dia->startOfWeek())->where('created_at', $dia->endOfWeek())->where('user_id', $meuID)->get();
