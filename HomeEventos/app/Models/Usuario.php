4c8b3b574989a5b934f299a6bc0f3f14ee059d84
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-02 00:19:46 +0200

    Update Usuario.php

--- HomeEventos/app/Models/Usuario.php
+++ HomeEventos/app/Models/Usuario.php
@@ -4,35 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\hash;
+
 
 class Usuario extends Model
 {
     use HasFactory;
-    public function atualizar(Request $request)
-    {
-        // $criptografar = Hash::make($request['senhaactual']);
-        // $query_ru = user::where(auth()->user()->password, $criptografar);
-        $check = hash::check($request['senhaactual'], auth()->user()->password);
-        if (!$check == true) {
-            return redirect()->back()->withErrors('A sua senha Actual Nao confere ');
-        } else {
-
-            $password = $request->password;
-            $password_confirmation = $request->password_confirmation;
-
-            if ($password == $password_confirmation) {
-                // $user = auth()->user()->id;
-                // // $user = User::findOrfail($user);
-                // Hash::make($request['password']);
-                // auth()->user()->id;
-                // $user->update('password', $request->password);
-                DB::table('users')->where(['id' => auth()->user()->id])->update(['password' => Hash::make($request->password)]);
-
-                return redirect()->back()->with('status', 'Senha Actualizada com Successos.');
-            } else {
-                return redirect()->back()->withErrors('suas credencias nao  batem com os nossos registos');
-            }
-        }
-    }
 }
