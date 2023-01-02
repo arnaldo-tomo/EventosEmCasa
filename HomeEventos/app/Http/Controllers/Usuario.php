3b892fa9473130fc6a18ed4ebbf2d8d76dbe80ce
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-02 11:50:30 +0200

    Update Usuario.php

--- HomeEventos/app/Http/Controllers/Usuario.php
+++ HomeEventos/app/Http/Controllers/Usuario.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\hash;
 
@@ -11,15 +12,16 @@ class Usuario extends Controller
     public function PasswordUpdate(Request $request)
     {
         // validadacao de campos
-        $request->validate([
-            'senhaactual' => 'required',
-            'password' => 'required',
-            'password_confirmation' => 'required',
-        ]);
+        // $request->validate([
+        //     'senhaactual' => 'required',
+        //     'password' => 'required',
+        //     'password_confirmation' => 'required',
+        // ]);
         // $criptografar = Hash::make($request['senhaactual']);
         // $query_ru = user::where(auth()->user()->password, $criptografar);
         $check = hash::check($request['senhaactual'], auth()->user()->password);
         if (!$check == true) {
+
             return redirect()->back()->withErrors('senhaactual', 'A sua senha Actual Nao confere ');
         } else {
 
