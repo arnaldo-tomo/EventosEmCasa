d5ce990ffa7953ce493693389dd662f9b7f52794
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-02 13:36:36 +0200

    Update Configuracoes.blade.php

--- HomeEventos/resources/views/eventos/Configuracoes.blade.php
+++ HomeEventos/resources/views/eventos/Configuracoes.blade.php
@@ -429,7 +429,7 @@ Header END -->
                                         <div class="col-sm-6 col-lg-6">
                                             <label class="form-label">Last name</label>
                                             <input type="text" class="form-control" placeholder=""
-                                                value="Lanson">
+                                                value="{{ Auth::user()->name }}">
                                         </div>
                                         <!-- Additional name -->
                                         <div class="col-sm-4 col-lg-4">
@@ -440,7 +440,7 @@ Header END -->
                                         <div class="col-sm-4">
                                             <label class="form-label">User name</label>
                                             <input type="text" class="form-control" placeholder=""
-                                                value="@samlanson">
+                                                value="{{ Auth::user()->name }}">
                                         </div>
                                         <!-- Birthday -->
                                         <div class="col-lg-4">
@@ -461,7 +461,7 @@ Header END -->
                                         <div class="col-sm-6">
                                             <label class="form-label">Phone number</label>
                                             <input type="text" class="form-control" placeholder=""
-                                                value="(678) 324-1251">
+                                                value="{{ auth::user()->contacto }}">
                                             <!-- Add new number -->
                                             <a class="btn btn-sm btn-dashed rounded mt-2" href="#!"> <i
                                                     class="bi bi-plus-circle-dotted me-1"></i>Add new phone number</a>
@@ -470,7 +470,7 @@ Header END -->
                                         <div class="col-sm-6">
                                             <label class="form-label">Email</label>
                                             <input type="text" class="form-control" placeholder=""
-                                                value="sam@webestica.com">
+                                                value="{{ auth::user()->email }}">
                                             <!-- Add new email -->
                                             <a class="btn btn-sm btn-dashed rounded mt-2" href="#!"> <i
                                                     class="bi bi-plus-circle-dotted me-1"></i>Add new email address</a>
@@ -478,7 +478,7 @@ Header END -->
                                         <!-- Page information -->
                                         <div class="col-12">
                                             <label class="form-label">Overview</label>
-                                            <textarea class="form-control" rows="4" placeholder="Description (Required)">Interested has all Devonshire difficulty gay assistance joy. Handsome met debating sir dwelling age material. As style lived he worse dried. Offered related so visitors we private removed. Moderate do subjects to distance.</textarea>
+                                            <textarea class="form-control" rows="4" placeholder="Description (Required)">{{ auth::user()->descricao }}</textarea>
                                             <small>Character limit: 300</small>
                                         </div>
                                         <!-- Button  -->
