50138a6e4fcef682a4ae1527a4cc63502f28632b
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-10 15:16:29 +0200

    Update Configuracoes.blade.php

--- HomeEventos/resources/views/eventos/Configuracoes.blade.php
+++ HomeEventos/resources/views/eventos/Configuracoes.blade.php
@@ -522,7 +522,7 @@ Header END -->
                                                 class="form-control @error('localizacao') is-invalid @enderror"
                                                 placeholder="Moçambique" value="{{ auth::user()->localizacao }}">
                                             <!-- Add new email -->
-                                            @if ($errors->==('localizacao'))
+                                            @if ($errors == 'localizacao')
                                                 @error('localizacao')
                                                     <span class="invalid-feedback" role="alert">
                                                         <strong>{{ $message }}</strong>
@@ -540,7 +540,7 @@ Header END -->
                                             <label class="form-label">Visão geral</label>
                                             <textarea class="form-control @error('descricao') is-invalid @enderror" name="descricao" rows="4"
                                                 placeholder="Description (Required)">{{ auth::user()->descricao }}</textarea>
-                                            @if ($errors==('descricao'))
+                                            @if ($errors == 'descricao')
                                                 @error('descricao')
                                                     <span class="invalid-feedback" role="alert">
                                                         <strong>{{ $message }}</strong>
