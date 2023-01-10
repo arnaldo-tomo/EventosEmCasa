cfd3686aae7ea8f35479f5c97943034309dc85c7
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-11 01:43:08 +0200

    Update verperfil.blade.php

--- HomeEventos/resources/views/eventos/verperfil.blade.php
+++ HomeEventos/resources/views/eventos/verperfil.blade.php
@@ -345,8 +345,8 @@ Header END -->
                             </div>
                             <!-- List profile -->
                             <ul class="list-inline mb-0 text-center text-sm-start mt-3 mt-sm-0">
-                                <li class="list-inline-item"><i class="bi bi-building me-1"></i> Lead Developer</li>
-                                <li class="list-inline-item"><i class="bi bi-geo-alt me-1"></i> Beira,Moçambique</li>
+                                <li class="list-inline-item"><i class="bi bi-building me-1"></i> {{ $info->empresa }} </li>
+                                <li class="list-inline-item"><i class="bi bi-geo-alt me-1"></i> {{ $info->localizacao }} </li>
                                 <li class="list-inline-item"><i class="bi bi-calendar2-plus me-1"></i>
                                     30 nov 2022
                                 </li>
