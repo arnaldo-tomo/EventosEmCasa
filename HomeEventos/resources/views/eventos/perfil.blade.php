a07b0a3b464d6d9a0a872ea2d308308d8887a630
Arnaldo jose Tomo <arnaldotomo@gmail.com>
2023-01-10 11:09:27 +0200

    Update perfil.blade.php

--- HomeEventos/resources/views/eventos/perfil.blade.php
+++ HomeEventos/resources/views/eventos/perfil.blade.php
@@ -265,6 +265,63 @@ Header END -->
 
         <!-- Container START -->
         <div class="container">
+            {{-- Modal --}}
+            <div class="modal fade" id="feedActionPhoto" tabindex="-1" aria-labelledby="feedActionPhotoLabel"
+                aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <!-- Modal feed header START -->
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="feedActionPhotoLabel">Add
+                                post photo</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                        </div>
+                        <!-- Modal feed header END -->
+
+                        <!-- Modal feed body START -->
+                        <div class="modal-body">
+                            <!-- Add Feed -->
+                            <div class="d-flex mb-3">
+                                <!-- Avatar -->
+                                <div class="avatar avatar-xs me-2">
+                                    <img class="avatar-img rounded-circle" src="assets/images/avatar/03.jpg"
+                                        alt="">
+                                </div>
+                                <!-- Feed box  -->
+                                <form class="w-100">
+                                    <textarea class="form-control pe-4 fs-3 lh-1 border-0" rows="2" placeholder="Share your thoughts..."></textarea>
+                                </form>
+                            </div>
+
+                            <!-- Dropzone photo START -->
+                            <div>
+                                <label class="form-label">Upload attachment</label>
+                                <div class="dropzone dropzone-default card shadow-none"
+                                    data-dropzone='{"maxFiles":2}'>
+                                    <div class="dz-message">
+                                        <i class="bi bi-images display-3"></i>
+                                        <p>Drag here or click to upload photo.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Dropzone photo END -->
+
+                        </div>
+                        <!-- Modal feed body END -->
+
+                        <!-- Modal feed footer -->
+                        <div class="modal-footer ">
+                            <!-- Button -->
+                            <button type="button" class="btn btn-danger-soft me-2"
+                                data-bs-dismiss="modal">Cancel</button>
+                            <button type="button" class="btn btn-success-soft">Post</button>
+                        </div>
+                        <!-- Modal feed footer -->
+                    </div>
+                </div>
+            </div>
+            {{-- Modal --}}
             <div class="row g-4">
 
                 <!-- Sidenav START -->
@@ -306,67 +363,7 @@ Header END -->
                                                         class="avatar-img rounded border border-white border-3"
                                                         src="assets/images/avatar/07.jpg" alt=""></a>
 
-                                                {{-- Modal --}}
-                                                <div class="modal fade" id="feedActionPhoto" tabindex="-1"
-                                                    aria-labelledby="feedActionPhotoLabel" aria-hidden="true">
-                                                    <div class="modal-dialog modal-dialog-centered">
-                                                        <div class="modal-content">
-                                                            <!-- Modal feed header START -->
-                                                            <div class="modal-header">
-                                                                <h5 class="modal-title" id="feedActionPhotoLabel">Add
-                                                                    post photo</h5>
-                                                                <button type="button" class="btn-close"
-                                                                    data-bs-dismiss="modal"
-                                                                    aria-label="Close"></button>
-                                                            </div>
-                                                            <!-- Modal feed header END -->
 
-                                                            <!-- Modal feed body START -->
-                                                            <div class="modal-body">
-                                                                <!-- Add Feed -->
-                                                                <div class="d-flex mb-3">
-                                                                    <!-- Avatar -->
-                                                                    <div class="avatar avatar-xs me-2">
-                                                                        <img class="avatar-img rounded-circle"
-                                                                            src="assets/images/avatar/03.jpg"
-                                                                            alt="">
-                                                                    </div>
-                                                                    <!-- Feed box  -->
-                                                                    <form class="w-100">
-                                                                        <textarea class="form-control pe-4 fs-3 lh-1 border-0" rows="2" placeholder="Share your thoughts..."></textarea>
-                                                                    </form>
-                                                                </div>
-
-                                                                <!-- Dropzone photo START -->
-                                                                <div>
-                                                                    <label class="form-label">Upload attachment</label>
-                                                                    <div class="dropzone dropzone-default card shadow-none"
-                                                                        data-dropzone='{"maxFiles":2}'>
-                                                                        <div class="dz-message">
-                                                                            <i class="bi bi-images display-3"></i>
-                                                                            <p>Drag here or click to upload photo.</p>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                                <!-- Dropzone photo END -->
-
-                                                            </div>
-                                                            <!-- Modal feed body END -->
-
-                                                            <!-- Modal feed footer -->
-                                                            <div class="modal-footer ">
-                                                                <!-- Button -->
-                                                                <button type="button"
-                                                                    class="btn btn-danger-soft me-2"
-                                                                    data-bs-dismiss="modal">Cancel</button>
-                                                                <button type="button"
-                                                                    class="btn btn-success-soft">Post</button>
-                                                            </div>
-                                                            <!-- Modal feed footer -->
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                {{-- Modal --}}
                                             </div>
                                             <!-- Info -->
                                             <h5 class="mb-0"> <a>{{ auth::user()->name }} </a> </h5>
